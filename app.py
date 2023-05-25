9d087e9376b6bdc87d3670518d5617af89d1c9d5
akash vaidya <akashvaidya132@gmail.com>
2023-05-25 14:02:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/api/data')
+def get_data():
+    data = {'name': 'John', 'age': 30}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
